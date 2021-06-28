1fc78d51bd8cced2a54714a71686cffad6b713e3
joshmanifold <86366869+joshmanifold@users.noreply.github.com>
2021-06-28 16:55:42 -0500

    Added main.tf

---
+++ main.tf
@@ -0,0 +1,54 @@
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       # The "hashicorp" namespace is the new home for the HashiCorp-maintained
+#       # provider plugins.
+#       #
+#       # source is not required for the hashicorp/* namespace as a measure of
+#       # backward compatibility for commonly-used providers, but recommended for
+#       # explicitness.
+#       source  = "hashicorp/azurerm"
+#       version = "~> 2.5.0"
+#       features {}
+#     }
+#   }
+# }
+
+#TODO: Revisit Role Assignments. 52:30 Azure DevOps: Provision API Infrastrcture using Terraform
+
+terraform {
+    required_providers {
+        azurerm = {
+        source = "hashicorp/azurerm"
+        version = "2.5.0"
+        }
+    }
+}
+
+provider "azurerm" {
+    features {}
+}
+
+resource "azurerm_resource_group" "tf_test" {
+    name = "tfmainrg"
+    location = "North Central US"
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+    name = "weatherapi"
+    location = azurerm_resource_group.tf_test.location
+    resource_group_name = azurerm_resource_group.tf_test.name
+    ip_address_type = "public"
+    dns_name_label = "joshmanifoldwa"
+    os_type = "Linux"
+    container {
+        name = "weatherapi"
+        image = "joshmanifold/weatherapi"
+        cpu = "1"
+        memory = "1"
+        ports {
+            port = 80
+            protocol = "TCP"
+        }
+    }
+}
